70bd533c076a21339cc4461074d5e3a77ee1631d
Max <chestm007@hotmail.com>
2019-04-18 18:56:56 +1000

    probably do some dev builds?

--- get_build_version.py
+++ get_build_version.py
@@ -2,4 +2,4 @@ import os
 import time
 
 last_tag = os.popen('git describe --abbrev=0 --tags').read().strip()
-print(os.environ.get('TRAVIS_TAG') or '{}.post{}'.format(last_tag, int(time.time())))
+print(os.environ.get('CIRCLE_TAG') or '{}.post{}'.format(last_tag, int(time.time())))
