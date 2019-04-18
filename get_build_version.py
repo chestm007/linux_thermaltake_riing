b4d873d568d739cb06012e333744e32d2f622520
Max <chestm007@hotmail.com>
2019-04-18 19:00:24 +1000

    name packages after branch

--- get_build_version.py
+++ get_build_version.py
@@ -1,5 +1,12 @@
 import os
 import time
 
-last_tag = os.popen('git describe --abbrev=0 --tags').read().strip()
-print(os.environ.get('CIRCLE_TAG') or '{}.post{}'.format(last_tag, int(time.time())))
+
+tag = os.environ.get('CIRCLE_TAG')
+if tag:
+    print(tag)
+else:
+    last_tag = os.popen('git describe --abbrev=0 --tags').read().strip()
+    cur_epoch = int(time.time())
+    cur_branch = os.environ.get('CIRCLE_BRANCH')
+    print('{}-{}.post{}'.format(last_tag, cur_branch, cur_epoch))
